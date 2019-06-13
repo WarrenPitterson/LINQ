5c899d7f65dbebd89449f0cea60e2ad2f4ec58c8
Warren Pitterson <warren.pitterson@kaleidaltd.co.uk>
2019-06-13 10:03:14 +0100

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+            ShowLargeFilesWithLinq(path);
+
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20}, : {file.Length,-10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInforComparer());
+
+            for(int i = 0; i < 5; i++) 
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20}, : {file.Length, -10:N0}");
+            }
+        }
+    }
+
+    public class FileInforComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
